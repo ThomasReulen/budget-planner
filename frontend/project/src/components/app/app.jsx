0cbe40751a014d443300632e029759ae55bca1a2
Thomas Reulen <reulen@contens.de>
2023-07-12 07:35:53 +0200

    first html

--- frontend/project/src/components/app/app.jsx
+++ frontend/project/src/components/app/app.jsx
@@ -1,11 +1,13 @@
 
 import { NavBar } from './nav';
 import { SideBar } from './sidebar';
+import { Bookings } from './bookings';
 
 export function App() {    
 
      return <>
                 <NavBar></NavBar>
                 <SideBar></SideBar>
+                <Bookings></Bookings>
             </>;
 }
